6d2b3c995b38f95e8be35036eee3cb127e6634eb
hzqianfeng <hzqianfeng@corp.netease.com>
2018-04-20 16:26:44 +0800

    Print auc in ftrl training

--- angel-ps/mllib/src/main/java/com/tencent/angel/ml/classification/ftrllr/FTRLLRLearner.scala
+++ angel-ps/mllib/src/main/java/com/tencent/angel/ml/classification/ftrllr/FTRLLRLearner.scala
@@ -70,6 +70,8 @@ class FTRLLRLearner(override val ctx: TaskContext) extends MLLearner(ctx) {
 
     globalMetrics.addMetric(MLConf.TRAIN_LOSS, LossMetric(trainData.size))
     globalMetrics.addMetric(MLConf.VALID_LOSS, LossMetric(valiData.size()))
+    globalMetrics.addMetric(MLConf.TRAIN_AUC, LossMetric(1))
+    globalMetrics.addMetric(MLConf.VALID_AUC, LossMetric(1))
 
     for (epoch <- 0 until epochNum) {
       LOG.info(s"Task[${ctx.getTaskId.getIndex}] Start epoch $epoch")
@@ -111,6 +113,7 @@ class FTRLLRLearner(override val ctx: TaskContext) extends MLLearner(ctx) {
       s"trueRecall = ${trainMetrics._4} " +
       s"falseRecall = ${trainMetrics._5}")
     globalMetrics.metric(MLConf.TRAIN_LOSS, trainMetrics._1)
+    globalMetrics.metric(MLConf.TRAIN_AUC, trainMetrics._3)
 
     if (valiData.size > 0) {
       val validMetric = ValidationUtils.calMetrics(valiData, weight, l1LL);
@@ -121,6 +124,7 @@ class FTRLLRLearner(override val ctx: TaskContext) extends MLLearner(ctx) {
         s"trueRecall=${validMetric._4} " +
         s"falseRecall=${validMetric._5}")
       globalMetrics.metric(MLConf.VALID_LOSS, validMetric._1)
+      globalMetrics.metric(MLConf.VALID_AUC, validMetric._3)
     }
   }
 }
